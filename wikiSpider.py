b34d981514c85f06db3d603ad285ce6a14c75749
Brian Creeden <brian.creeden@gmail.com>
2015-02-28 12:43:50 -0500

    Now that I better understand the optimal way to parse the different types of links included on the Wikipedia desalination page, I'm scoping the initial spider down to include only internal Wikipedia links with a depth_setting of 2.
    I'll return to the external links after I have a functional
    internal spider. If I want to do this properly, the PDF parser
    will require a pipeline, which I'll hold off on until the third
    phase of this project.

--- wikiSpider.py
+++ wikiSpider.py
@@ -1,36 +1,54 @@
+# Spider for crawling links relevant to Desalination, starting at the Wikipedia desalination page, and making
+#   this output available in a format that IBM Watson can consume
+#
+# The Wikipedia desalination page contains three types of links: internal, external, and PDF files
+# I'm going to start with the internal links, expand to extern, then (hopefully) grab and parse PDFs
+#
+# Effectively parsing PDFs appears to require a pipeline, a more complex way of managing spiders
+# I'm not going to mess with pipelines for this initial project, so I'll keep the scope of this spider to Wikipedia
+#
+# Author: Brian Creeden
+
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from scrapy.item import Item, Field
 
+from nltk import clean_html
+
+from docx import Document
+
 class PageItem(Item):
-    session_id = Field()
     depth = Field()
     current_url = Field()
-    referring_url = Field()
-    title = Field()
     format = Field()
+    title = Field()
+    body = Field()
 
 class DesalinationSpider(CrawlSpider):
     name = 'desalination'
     start_urls = ["http://en.wikipedia.org/wiki/Desalination"]
-    rules = ( Rule (SgmlLinkExtractor(allow=("", ),),
-                    callback="parse_items", follow= True),
-    )
 
-    def __init__(self, session_id=-1, *args, **kwargs):
+    # pass responses to either parse_html or parse_pdf depending on whether URLs end in .html and .pdf
+
+    rules = [
+        Rule(SgmlLinkExtractor(allow=('\.html', )), callback='parse_html'),
+        Rule(SgmlLinkExtractor(allow=('\.pdf', )), callback='parse_pdf')
+    ]
+
+    def __init__(self, *args, **kwargs):
         super(DesalinationSpider, self).__init__(*args, **kwargs)
-        self.session_id = session_id
 
-    def parse_items(self, response):
+    def parse_html(self, response):
         sel = Selector(response)
         items = []
         item = PageItem()
-        item['session_id'] = self.session_id
         item['depth'] = response.meta['depth']
         item['current_url'] = response.url
-        referring_url = response.request.headers.get('Referer', None)
-        item['referring_url'] = referring_url
-        item['title'] = sel.xpath('').extract()
+        item['title'] = sel.xpath('//head/title/text()').extract()
+        item['body'] = clean_html(sel.xpath('//body'))
         items.append(item)
         return items
+
+    def parse_pdf(self, response):
+        sel = Selector(response)
